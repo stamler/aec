fabca44633cbbc56261a626c3f81bcb47b11ca46
Dean Stamler <dean@stamler.ca>
2020-12-05 21:37:22 -0500

    code reorg, types, TODO

--- backup.py
+++ backup.py
@@ -6,14 +6,17 @@ import os
 import datetime
 import shutil
 
-if len(sys.argv) == 2:
-  dir_name = sys.argv[1]
-  # check if dir_name is a directory
-  if os.path.isdir(dir_name):
-    output_filename = f"aec_uploads_{ datetime.datetime.now(tz=datetime.timezone.utc).strftime('%Y-%m-%d_%H:%M:%SUTC') }" 
-    shutil.make_archive(output_filename, 'zip', dir_name)
-    print (f"Created {output_filename}")
-  else:
-    print ("The provided argument is not a directory")
-else:
+if len(sys.argv) != 2:
   print ("\nUsage: backup.py <dirname>\n")
+
+dir_name: str = sys.argv[1]
+
+# check if dir_name is a directory
+if not os.path.isdir(dir_name):
+  print ("The provided argument is not a directory")
+
+output_filename: str = f"aec_uploads_{ datetime.datetime.now(tz=datetime.timezone.utc).strftime('%Y-%m-%d_%H:%M:%SUTC') }" 
+shutil.make_archive(output_filename, 'zip', dir_name)
+# TODO: calculate checksum and include it in the filename
+# TODO: delete older backups beyond a certain number
+print (f"Created {output_filename}")
