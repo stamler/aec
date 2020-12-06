4eba049f511a99f686dcdaeae2d3f307bfa55677
Dean Stamler <dean@stamler.ca>
2020-12-05 20:27:34 -0500

    fix double extension

--- backup.py
+++ backup.py
@@ -8,7 +8,7 @@ if len(sys.argv) == 2:
   dir_name = sys.argv[1]
   # check if dir_name is a directory
   if os.path.isdir(dir_name):
-    output_filename = f"aec_uploads_{ datetime.datetime.now(tz=datetime.timezone.utc).strftime('%Y-%m-%d_%H:%M:%SUTC') }.zip" 
+    output_filename = f"aec_uploads_{ datetime.datetime.now(tz=datetime.timezone.utc).strftime('%Y-%m-%d_%H:%M:%SUTC') }" 
     shutil.make_archive(output_filename, 'zip', dir_name)
     print (f"Created {output_filename}")
   else:
